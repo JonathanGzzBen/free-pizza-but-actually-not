a16b5a85713db12617be162249ba159307e4d492
JonathanGzzBen <jonathangzzben@gmail.com>
2021-03-23 12:49:39 -0600

    Install react-bootstrap

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import Layout from "../components/layout";
+import "../styles/globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Col, Container, Row, Image } from "react-bootstrap";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Container fluid>
+        <Row className="justify-content-center">
+          <Col className="justify-content-center" sm={8}>
+            <Container fluid className="pt-4">
+              <Image src="https://via.placeholder.com/150"></Image>
+              <p>You can say this is the home</p>
+            </Container>
+          </Col>
+        </Row>
+      </Container>
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
