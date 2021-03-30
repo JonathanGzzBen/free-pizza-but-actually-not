7f6b8693755547dada36a4674915036849a56f9d
JonathanGzzBen <jonathangzzben@gmail.com>
2021-03-29 21:46:26 -0600

    Add state to pay-order page

--- src/pages/pay-order.jsx
+++ src/pages/pay-order.jsx
@@ -1,96 +1,120 @@
+import React from "react";
 import { Row, Col, Form, Button, CardColumns } from "react-bootstrap";
 import Layout from "../components/layout";
 
-export default function PayOrder() {
-  return (
-    <Layout>
-      <Form>
-        <Row>
-          <Col>
-            <Form.Group controlId="folio">
-              <Form.Label>FOLIO:</Form.Label>
-              <Form.Control type="text" ReadOnly value="001" />
-            </Form.Group>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Form.Group controlId="cliente">
-              <Form.Label>CLIENTE:</Form.Label>
-              <Form.Control type="text" ReadOnly placeholder="Ingrese nombre" />
-            </Form.Group>
-          </Col>
-          <Col>
-            <Form.Group controlId="telefono">
-              <Form.Label>TELEFONO:</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Ingrese telefono"
-                readOnly
-              />
-            </Form.Group>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Form.Group controlId="direccion">
-              <Form.Label>DIRECCCION:</Form.Label>
-              <Form.Control
-                as="textarea"
-                style={{ resize: "none" }}
-                placeholder="Ingrese direccion"
-                rows="5"
-                readOnly
-              />
-              <Form.Text className="text-muted">
-                Agregue todos los detalles que necesitemos para llegar.
-              </Form.Text>
-            </Form.Group>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Form.Group controlId="pedido">
-              <Form.Label>PEDIDO:</Form.Label>
-              <Form.Control
-                as="textarea"
-                style={{ resize: "none" }}
-                rows="5"
-                value="Resumen del pedido"
-                readOnly
-              />
-            </Form.Group>
-          </Col>
-          <Col>
-            <Form.Group controlId="forma-pago">
-              <Form.Label>FORMA DE PAGO:</Form.Label>
-              <Form.Control type="text" readOnly />
-            </Form.Group>
-            <Form.Group controlId="subtotal">
-              <Form.Label>SUB-TOTAL:</Form.Label>
-              <Form.Control type="text" readOnly />
-            </Form.Group>
-            <Form.Group controlId="iva">
-              <Form.Label>IVA:</Form.Label>
-              <Form.Control type="text" readOnly />
-            </Form.Group>
-            <Form.Group controlId="total">
-              <Form.Label>TOTAL:</Form.Label>
-              <Form.Control type="text" readOnly />
-            </Form.Group>
-          </Col>
-        </Row>
-        <Row className="justify-content-end">
-          <Form.Group as={Row}>
-            <Col style={{ padding: "0" }} sm={{ span: 10 }}>
-              <Button type="submit">Cancelar</Button>
+export default class PayOrder extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      folio: "001",
+      cliente: "Nombre del cliente",
+      telefono: "Telefono del cliente",
+      direccion: "Direccion del cliente",
+      pedido: "Resumen del pedido",
+      formaPago: "Forma de pago (EFECTIVO)",
+      subTotal: "$subtotal",
+      iva: "$iva",
+      total: "$total",
+    };
+  }
+
+  render() {
+    return (
+      <Layout>
+        <Form>
+          <Row>
+            <Col>
+              <Form.Group controlId="folio">
+                <Form.Label>FOLIO:</Form.Label>
+                <Form.Control type="text" readOnly value={this.state.folio} />
+              </Form.Group>
             </Col>
-            <Col style={{ padding: "0" }} sm={{ span: 10 }}>
-              <Button type="submit">Listo</Button>
+          </Row>
+          <Row>
+            <Col>
+              <Form.Group controlId="cliente">
+                <Form.Label>CLIENTE:</Form.Label>
+                <Form.Control type="text" readOnly value={this.state.cliente} />
+              </Form.Group>
             </Col>
-          </Form.Group>
-        </Row>
-      </Form>
-    </Layout>
-  );
+            <Col>
+              <Form.Group controlId="telefono">
+                <Form.Label>TELEFONO:</Form.Label>
+                <Form.Control
+                  type="text"
+                  readOnly
+                  value={this.state.telefono}
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Form.Group controlId="direccion">
+                <Form.Label>DIRECCCION:</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  style={{ resize: "none" }}
+                  placeholder="Ingrese direccion"
+                  rows="5"
+                  readOnly
+                  value={this.state.direccion}
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Form.Group controlId="pedido">
+                <Form.Label>PEDIDO:</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  style={{ resize: "none" }}
+                  rows="5"
+                  value={this.state.pedido}
+                  readOnly
+                />
+              </Form.Group>
+            </Col>
+            <Col>
+              <Form.Group controlId="forma-pago">
+                <Form.Label>FORMA DE PAGO:</Form.Label>
+                <Form.Control
+                  type="text"
+                  readOnly
+                  value={this.state.formaPago}
+                />
+              </Form.Group>
+              <Form.Group controlId="sub-total">
+                <Form.Label>SUB-TOTAL:</Form.Label>
+                <Form.Control
+                  type="text"
+                  readOnly
+                  value={this.state.subTotal}
+                />
+              </Form.Group>
+              <Form.Group controlId="iva">
+                <Form.Label>IVA:</Form.Label>
+                <Form.Control type="text" readOnly value={this.state.iva} />
+              </Form.Group>
+              <Form.Group controlId="total">
+                <Form.Label>TOTAL:</Form.Label>
+                <Form.Control type="text" readOnly value={this.state.total} />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row className="justify-content-end">
+            <Form.Group as={Row}>
+              <Col style={{ padding: "0" }} sm={{ span: 10 }}>
+                <Button type="submit">Cancelar</Button>
+              </Col>
+              <Col style={{ padding: "0" }} sm={{ span: 10 }}>
+                <Button type="submit">Listo</Button>
+              </Col>
+            </Form.Group>
+          </Row>
+        </Form>
+      </Layout>
+    );
+  }
 }
