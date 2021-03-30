a26490464cc700d08b9560222d15f45f11de4bdb
JonathanGzzBen <jonathangzzben@gmail.com>
2021-03-30 15:37:10 -0600

    Modify signin page component to be a function component and use hooks

--- src/pages/signin.jsx
+++ src/pages/signin.jsx
@@ -1,71 +1,56 @@
-import React from "react";
+import { useState } from "react";
 import { Row, Col, Form, Button, CardColumns } from "react-bootstrap";
 import Layout from "../components/layout";
 
-export default class Order extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      usuario: "",
-      contraseña: "",
-    };
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleLoginSubmitClick = this.handleLoginSubmitClick.bind(this);
-  }
+export default function Order() {
+  const [usuario, setUsuario] = useState("");
+  const [contraseña, setContraseña] = useState("");
 
-  handleInputChange(e) {
-    this.setState({
-      [e.target.name]: e.target.value,
-    });
-    console.log(this.state);
-  }
-  handleLoginSubmitClick(e) {
+  const handleLoginSubmitClick = (e) => {
     e.preventDefault();
     alert(
       "Iniciando sesion con el usuario " +
-        this.state.usuario +
+        usuario +
         " y la contraseña " +
-        this.state.contraseña
+        contraseña
     );
-  }
+  };
 
-  render() {
-    return (
-      <Layout>
-        <Form>
-          <Row className="justify-content-center">
-            <Col md={5}>
-              <Form.Group controlId="usuario">
-                <Form.Label>👤USUARIO:</Form.Label>
-                <Form.Control
-                  type="text"
-                  name="usuario"
-                  onChange={this.handleInputChange}
-                  placeholder="Ingrese su usuario"
-                />
+  return (
+    <Layout>
+      <Form>
+        <Row className="justify-content-center">
+          <Col md={5}>
+            <Form.Group controlId="usuario">
+              <Form.Label>👤USUARIO:</Form.Label>
+              <Form.Control
+                type="text"
+                value={usuario}
+                onChange={(e) => setUsuario(e.target.value)}
+                placeholder="Ingrese su usuario"
+              />
+            </Form.Group>
+            <Form.Group controlId="contrasena">
+              <Form.Label>🔒CONTRASENA:</Form.Label>
+              <Form.Control
+                type="password"
+                value={contraseña}
+                onChange={(e) => setContraseña(e.target.value)}
+                placeholder="Ingrese su contrasena"
+              />
+            </Form.Group>
+            <Row className="justify-content-center">
+              <Form.Group>
+                <Col sm={{ span: 10 }}>
+                  <Button type="submit" onClick={handleLoginSubmitClick}>
+                    Iniciar Sesion
+                  </Button>
+                </Col>
               </Form.Group>
-              <Form.Group controlId="contrasena">
-                <Form.Label>🔒CONTRASENA:</Form.Label>
-                <Form.Control
-                  type="password"
-                  name="contraseña"
-                  onChange={this.handleInputChange}
-                  placeholder="Ingrese su contrasena"
-                />
-              </Form.Group>
-              <Row className="justify-content-center">
-                <Form.Group>
-                  <Col sm={{ span: 10 }}>
-                    <Button type="submit" onClick={this.handleLoginSubmitClick}>
-                      Iniciar Sesion
-                    </Button>
-                  </Col>
-                </Form.Group>
-              </Row>
-            </Col>
-          </Row>
-        </Form>
-      </Layout>
-    );
-  }
+            </Row>
+          </Col>
+        </Row>
+      </Form>
+    </Layout>
+  );
 }
